d32642d3a4f53b3d4bbf5df7900ea8957ac14533
Pol <pol.surriel@enti.cat>
2022-06-04 00:34:11 +0200

    fixed :-1 bug

--- training.py
+++ training.py
@@ -5,7 +5,7 @@ from forward_porpagation import forward_propagate
 # Update network weights with error
 def update_weights(network, row, l_rate):
     for i in range(len(network)):
-        inputs = row
+        inputs = row[:]
         if i != 0:
             inputs = [neuron['output'] for neuron in network[i - 1]]
         for neuron in network[i]:
