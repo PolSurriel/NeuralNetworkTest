cc5ea3a04fc9a8f04972b8e0458458218a190d6f
Pol <pol.surriel@enti.cat>
2022-06-05 11:16:54 +0200

    working

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4598df835c68a5bc7b1bf10deaa3097416e9e30df8aed0506bfa70d8231dcc9b
-size 1664
+oid sha256:2ba019e0c4d0779bb919a4d76b1da3a3eca2b54cbb0f08c3a2caba9e6cc006bd
+size 1645
